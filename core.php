ff74d4846023f79b6904f5851a0c1df5f64e7afd
Jefff <jeff@nullus.io>
2021-09-01 18:20:35 +0100

    new functions

--- core.php
+++ core.php
@@ -1,60 +1,63 @@
 <?php
 class core {
     protected $connection;
-    protected $query;
+	protected $query;
     protected $show_errors = TRUE;
     protected $query_closed = TRUE;
-    public $query_count = 0;
-    public function __construct($dbhost = 'ip', $dbuser = 'user', $dbpass = 'pass', $dbname = 'dbname', $charset = 'utf8') {
-        $this->connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-        if ($this->connection->connect_error) {
-            $this->error('Failed to connect to MySQL - ' . $this->connection->connect_error);
-        }
-        $this->connection->set_charset($charset);
-    }
+	public $query_count = 0;
+
+	public function __construct($dbhost = 'db', $dbuser = 'db', $dbpass = 'db', $dbname = 'db', $charset = 'utf8') {
+		$this->connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+		if ($this->connection->connect_error) {
+			$this->error('Failed to connect to MySQL - ' . $this->connection->connect_error);
+		}
+		$this->connection->set_charset($charset);
+	}
+
     public function query($query) {
         if (!$this->query_closed) {
             $this->query->close();
         }
-        if ($this->query = $this->connection->prepare($query)) {
+		if ($this->query = $this->connection->prepare($query)) {
             if (func_num_args() > 1) {
                 $x = func_get_args();
                 $args = array_slice($x, 1);
-                $types = '';
+				$types = '';
                 $args_ref = array();
-                foreach ($args as $k => & $arg) {
-                    if (is_array($args[$k])) {
-                        foreach ($args[$k] as $j => & $a) {
-                            $types.= $this->_gettype($args[$k][$j]);
-                            $args_ref[] = & $a;
-                        }
-                    } else {
-                        $types.= $this->_gettype($args[$k]);
-                        $args_ref[] = & $arg;
-                    }
+                foreach ($args as $k => &$arg) {
+					if (is_array($args[$k])) {
+						foreach ($args[$k] as $j => &$a) {
+							$types .= $this->_gettype($args[$k][$j]);
+							$args_ref[] = &$a;
+						}
+					} else {
+	                	$types .= $this->_gettype($args[$k]);
+	                    $args_ref[] = &$arg;
+					}
                 }
-                array_unshift($args_ref, $types);
+				array_unshift($args_ref, $types);
                 call_user_func_array(array($this->query, 'bind_param'), $args_ref);
             }
             $this->query->execute();
-            if ($this->query->errno) {
-                $this->error('Unable to process MySQL query (check your params) - ' . $this->query->error);
-            }
+           	if ($this->query->errno) {
+				$this->error('Unable to process MySQL query (check your params) - ' . $this->query->error);
+           	}
             $this->query_closed = FALSE;
-            $this->query_count++;
+			$this->query_count++;
         } else {
             $this->error('Unable to prepare MySQL statement (check your syntax) - ' . $this->connection->error);
         }
-        return $this;
+		return $this;
     }
-    public function fetchAll($callback = null) {
-        $params = array();
+	
+	public function fetchAll($callback = null) {
+	    $params = array();
         $row = array();
-        $meta = $this->query->result_metadata();
-        while ($field = $meta->fetch_field()) {
-            $params[] = & $row[$field->name];
-        }
-        call_user_func_array(array($this->query, 'bind_result'), $params);
+	    $meta = $this->query->result_metadata();
+	    while ($field = $meta->fetch_field()) {
+	        $params[] = &$row[$field->name];
+	    }
+	    call_user_func_array(array($this->query, 'bind_result'), $params);
         $result = array();
         while ($this->query->fetch()) {
             $r = array();
@@ -70,64 +73,114 @@ class core {
         }
         $this->query->close();
         $this->query_closed = TRUE;
-        return $result;
-    }
-    public function fetchArray() {
-        $params = array();
+		return $result;
+	}
+
+	public function fetchArray() {
+	    $params = array();
         $row = array();
-        $meta = $this->query->result_metadata();
-        while ($field = $meta->fetch_field()) {
-            $params[] = & $row[$field->name];
-        }
-        call_user_func_array(array($this->query, 'bind_result'), $params);
+	    $meta = $this->query->result_metadata();
+	    while ($field = $meta->fetch_field()) {
+	        $params[] = &$row[$field->name];
+	    }
+	    call_user_func_array(array($this->query, 'bind_result'), $params);
         $result = array();
-        while ($this->query->fetch()) {
-            foreach ($row as $key => $val) {
-                $result[$key] = $val;
-            }
-        }
+		while ($this->query->fetch()) {
+			foreach ($row as $key => $val) {
+				$result[$key] = $val;
+			}
+		}
         $this->query->close();
         $this->query_closed = TRUE;
-        return $result;
-    }
-    public function close() {
-        return $this->connection->close();
-    }
+		return $result;
+	}
+
+	public function close() {
+		return $this->connection->close();
+	}
+
     public function numRows() {
-        $this->query->store_result();
-        return $this->query->num_rows;
-    }
-    public function affectedRows() {
-        return $this->query->affected_rows;
-    }
+		$this->query->store_result();
+		return $this->query->num_rows;
+	}
+
+	public function affectedRows() {
+		return $this->query->affected_rows;
+	}
+
     public function lastInsertID() {
-        return $this->connection->insert_id;
+    	return $this->connection->insert_id;
     }
+
     public function error($error) {
         if ($this->show_errors) {
             exit($error);
         }
     }
-    private function _gettype($var) {
-        if (is_string($var)) return 's';
-        if (is_float($var)) return 'd';
-        if (is_int($var)) return 'i';
-        return 'b';
-    }
-    public function downloadFile($file, $filename) {
-        if (file_exists($file)) {
-            header('Content-Description: File Transfer');
-            header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename="' . basename($filename) . '"');
-            header('Expires: 0');
-            header('Cache-Control: must-revalidate');
-            header('Pragma: public');
-            header('Content-Length: ' . filesize($file));
-            readfile($file);
-            //exit;
-            
-        }
-    }
+
+	private function _gettype($var) {
+	    if (is_string($var)) return 's';
+	    if (is_float($var)) return 'd';
+	    if (is_int($var)) return 'i';
+	    return 'b';
+	}
+	# -- End Database -- #
+	
+	# -- FUNCTIONS -- #
+	public function escape($string) {
+		return htmlspecialchars(mysqli_real_escape_string($this->connection, $string));
+	}
+	
+	public function password_encrypt($string) {
+		$return = hash('sha512', $string);
+		$return = password_hash($return, PASSWORD_DEFAULT);
+		return $return;
+	}
+	
+	public function password_check($plain, $encrypted) {
+		$return = hash('sha512', $plain);
+		return password_verify($return, $encrypted);
+	}
+
+	public function download ($file, $filename) {
+		if (file_exists($file)) {
+			header('Content-Description: File Transfer');
+			header('Content-Type: application/octet-stream');
+			header('Content-Disposition: attachment; filename="'.basename($filename).'"');
+			header('Expires: 0');
+			header('Cache-Control: must-revalidate');
+			header('Pragma: public');
+			header('Content-Length: ' . filesize($file));
+			readfile($file);
+		}
+	}
+	
+	public function redirect($url) {
+		if (!headers_sent())
+		{    
+			header('Location: '.$url);
+			exit;
+		}
+		else {  
+			echo '<script type="text/javascript">';
+			echo 'window.location.href="'.$url.'";';
+			echo '</script>';
+			echo '<noscript>';
+			echo '<meta http-equiv="refresh" content="0;url='.$url.'" />';
+			echo '</noscript>';
+			exit;
+		}
+	}
+	
+	public function random($n) { 
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+		$randomString = ''; 
+		for ($i = 0; $i < $n; $i++) { 
+			$index = rand(0, strlen($characters) - 1); 
+			$randomString .= $characters[$index]; 
+		} 
+		return $randomString; 
+	}
 }
 $core = new core();
 ?>
